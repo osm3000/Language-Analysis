37a965fa80ca82b7090c414797b42991a9222cc0
Omar Mohammed <omar.samir3000@gmail.com>
2022-03-11 15:53:56 +0100

    Abstracting the configuration file fetching mechansim --> much more flexible this way, allows for the handling of complex logic and conditions that were not possible before

---
+++ configurations.py
@@ -0,0 +1,28 @@
+import configparser
+import platform
+
+HOSTNAME = platform.node()
+config_machines = configparser.ConfigParser()
+config_machines.read("./config/config_machines.ini")
+
+
+def get_config_file():
+    config = configparser.ConfigParser()
+    if config_machines["MACHINES"][HOSTNAME] == "local":
+        config.read("./config/config_local.ini")
+    elif config_machines["MACHINES"][HOSTNAME] == "remote":
+        config.read("./config/config_remote.ini")
+    else:
+        config.read("./config/config_local.ini")
+
+    return config
+
+
+def get_secrets_file():
+    config = configparser.ConfigParser()
+    try:
+        config.read("./config/auth_lemonde.ini")
+    except:
+        config = {"auth": {"username": "", "password": ""}}
+        print("Valid authentication file not found")
+    return config

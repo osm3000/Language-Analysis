c498ee67b3853a8f7d6a18b32edfaa02a4a32d1c
Omar Mohammed <omar.samir3000@gmail.com>
2022-05-19 23:55:19 +0200

    I am not sure :D

--- extract_metadata.py
+++ extract_metadata.py
@@ -3,62 +3,55 @@ I want to extract the following:
 1. The date
 2. The topic/category to which this article belongs
 Those two I will extract mainly from the URL.
+
+UPDATE: 21/04/22
+I've remodified this code. Now it will do the following:
+- Extract the metadata mentioned earlier
+- Remove any weird articles (articles that don't have '.html' at the end of their url)
 """
 import json
 import os
+import configurations
+import utilities
 
-SRC_DIR = "./data_3"
-TGT_DIR = "./data_4"
+CONFIG = configurations.get_config_file()
 
 
 def main():
-    file_names = os.listdir(SRC_DIR)
-    all_topics = {}
-
-    for file_index, file_name in enumerate(file_names):
-        # if file_index >= 100:
-        #     break
-        # if file_index <= 11049:
-        # continue
-        # print("/-/" * 20)
-
-        if (file_name != "all_links_recorded.json") and (
-            file_name != "all_topic_collected.json"
-        ):
-            print(file_index, " -->> ", file_name)
-            article_data = json.load(open(f"{SRC_DIR}/{file_name}", "r"))
+    file_names = os.listdir(CONFIG["DIR_PATH"]["SRC_DIR"])
+    bad_articles = 0
+    with utilities.DataLoader() as data_loader:
+        for article_index, article_data in enumerate(data_loader):
             article_url = article_data["article_link"]
-            article_url_split = article_url.split("/")
-
-            article_url_topic = article_url_split[3]
-            article_url_date = article_url_split[5:8]
-
-            # print(f"Whole URL: {article_url}")
-            # print(f"Category: {article_url_topic}")
-            # print(f"Date: {article_url_date}")
-
+            file_name = article_data["file_name"]
+            if article_url[-5:] != ".html":
+                bad_articles += 1
+                print(f"Bad article: {bad_articles}")
+                continue
             try:
-                all_topics[article_url_topic] += 1
-            except:
-                all_topics[article_url_topic] = 1
+                article_url_split = article_url.split("/")
 
-            article_data["topic_category"] = article_url_topic
-            try:  # There can be very rare case, where it is not really an article, thus, doesn't follow the normal schema
+                article_url_topic = article_url_split[3]
+                article_url_date = article_url_split[5:8]
+
+                article_data["topic_category"] = article_url_topic
                 article_data["publication_date"] = {
                     "year": article_url_date[0],
                     "month": article_url_date[1],
                     "day": article_url_date[2],
                 }
-            except:
-                print("not a good article")
+
+                json.dump(
+                    article_data,
+                    open(f'{CONFIG["DIR_PATH"]["TGT_DIR"]}/{file_name}', "w"),
+                )
+            except Exception as e:
+                print(e)
+                print(file_name)
+                print(article_url_split)
+                bad_articles += 1
+                print(f"Bad article: {bad_articles}")
                 continue
-
-            json.dump(article_data, open(f"{TGT_DIR}/{file_name}", "w"))
-
-    json.dump(all_topics, open(f"{TGT_DIR}/all_topic_collected.json", "w"))
-    print(f"Nb of unique topics: {len(all_topics.keys())}")
-    for item in all_topics:
-        print(f"{item} -->> {all_topics[item]}")
 
 
 if __name__ == "__main__":
